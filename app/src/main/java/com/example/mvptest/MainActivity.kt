26bbba083f2215bd91db934cf3e1f772642624aa
Andrew <verannick@gmail.com>
2019-12-25 20:26:56 +0300

    Click handled with adapter in presenter

--- app/src/main/java/com/example/mvptest/MainActivity.kt
+++ app/src/main/java/com/example/mvptest/MainActivity.kt
@@ -8,30 +8,28 @@ import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 
 
-class MainActivity : MvpAppCompatActivity(),MainView,DataAdapter.ViewHolder.clickListener{
+class MainActivity : MvpAppCompatActivity(),MainView{
     @InjectPresenter
     lateinit var mainPresenter: MainPresenter
-    var adapter = DataAdapter(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recycler:RecyclerView = findViewById(R.id.dataList)
-        recycler.adapter=this.adapter
-        //askForAdapter(recycler)
+        askForAdapter(recycler)
         if (recycler.adapter?.itemCount==0)
             askForData()
     }
     override fun askForData() {
-        mainPresenter.setTheData(adapter)
+        mainPresenter.setTheData()
     }
 
-//    override fun askForAdapter(recyclerView: RecyclerView) {
-//        mainPresenter.setTheAdapter(recyclerView)
-//    }
+    override fun askForAdapter(recyclerView: RecyclerView) {
+        mainPresenter.setTheAdapter(recyclerView)
+    }
 
     override fun onItemClick(position: Int) {
         Log.d("Click","Click is happening")
-        mainPresenter.makeMessage(position,adapter)
+        mainPresenter.makeMessage(position)
     }
 
     override fun showMessage(text: String) {
